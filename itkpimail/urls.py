3b7db56f843bb8bde8a9108929209d91c7cb5f54
Roman Rader <roman.rader@gmail.com>
2015-07-06 21:24:09 +0300

    name

--- itkpimail/urls.py
+++ itkpimail/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
 
-admin.site.site_header = 'Sociomator'
+admin.site.site_header = 'IT KPI Maillist Generation Engine'
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/admin/')),
