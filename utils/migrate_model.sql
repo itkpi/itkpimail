e5f41f4ec1c223f86b75e00892f82178bd948745
Roman Rader <roman.rader@gmail.com>
2015-07-09 01:41:45 +0300

    instructions

--- utils/migrate_model.sql
+++ utils/migrate_model.sql
@@ -1,5 +1,13 @@
 == Migrating from default user model to custom ==
 
+1) Dump DB (just in case)
+2) Make initial migration with custom User model (Model should be called only User,
+not CustomUser or anything else to not break relation names) that has Meta: db_table='auth_user'
+3) Push code to heroku
+4) Run manage.py migrate --fake-initial
+so that now django thinks that we had custom user model from the beginning
+5) Alter User table
+
 \d auth_user
     Column    |           Type           |                       Modifiers
 --------------+--------------------------+--------------------------------------------------------
