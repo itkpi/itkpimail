6dd0ab23b2f499d727a09ed16ee242026c431427
Roman Rader <roman.rader@gmail.com>
2015-08-13 19:08:42 +0300

    Don't edit usernames!

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,4 +5,4 @@ from django import forms
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['first_name', 'last_name', 'email']
