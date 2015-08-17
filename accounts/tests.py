1788621be5d7be262c6006098da43b9be152f417
Roman Rader <roman.rader@gmail.com>
2015-08-17 12:54:32 +0300

    test command

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,7 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 # Create your tests here.
+class AccountTest(TestCase):
+    def test_home_accessible(self):
+        self.assertEqual(self.client.get(reverse('home_page')).status_code, 200)
